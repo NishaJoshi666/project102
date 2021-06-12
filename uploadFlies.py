304f2aa499ea9f50ac4177d8a93b2ccd79ba040f
Nisha Joshi <69041055+NishaJoshi666@users.noreply.github.com>
2021-06-12 11:26:51 +0530

    Update uploadFlies.py

--- uploadFlies.py
+++ uploadFlies.py
@@ -1,37 +1,36 @@
-import os
+import cv2
 import dropbox
-from dropbox.files import WriteMode
-#
-class TransferData:
-    def __init__(self,access_token):
-        self.access_token =  access_token
-#
-    def upload_file(self, file_from, file_to):
-        dbx = dropbox.Dropbox(self.access_token)
+import random
+import time
+startTime = time.time()
 
-            # enumerate local files recursively
-        for root, dirs, files in os.walk(file_from):
+def take_picture():
+    number = random.randint(0,100)
+    videoPicture = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        ret,frame = videoPicture.read()
+        imageName = 'picture'+ str(number) + '.png'
+        cv2.imwrite(imageName,frame)
+        result = False
+    return imageName
+    videoPicture.release()
+    cv2.destroyAllWindows()
+    print('picture has been captured')
 
-            for filename in files:
-                    # construct the full local path
-                local_path = os.path.join(root, filename)
-
-                    # construct the full Dropbox path
-                relative_path = os.path.relpath(local_path, file_from)
-                dropbox_path = os.path.join(file_to, relative_path)
-                    # upload the file
-                with open(local_path, 'rb') as f:
-                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+def uploadPics(imageName):
+    access_token = '_kdXQaWORScAAAAAAAAAAev3fi46cAMrii2jyYnETXDzuErkYiox6TyM0gDwssMe'
+    filefrom = imageName
+    fileto = '/Project102/'+imageName
+    dbx = dropbox.Dropbox(access_token)
+    with open(filefrom,'rb') as f:
+        dbx.files_upload(f.read(),fileto,mode = dropbox.files.WriteMode.overwrite)
+        print('file has been uploaded')
 
 def main():
-    access_token = 'riFu6Ybhc9AAAAAAAAAAHWkfE9AiGyD6n4254tOxesw7ShRjGjFhrjhRVa3NX3mx'
-    transferData = TransferData(access_token)
-
-    file_from = str(input("Enter the folder path to transfer : -"))
-    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
-
-    # API v2
-    transferData.upload_file(file_from,file_to)
-    print("file has been moved !!!")
+    while(True):
+        if((time.time()-startTime)>=300and(time.time()-startTime)%300==0):
+            name = take_snapshot()
+            uploadPics(name)
 
 main()
