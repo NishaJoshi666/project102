ff35d30b4102ea8b7b354a747402e64dcefc3fbe
Nisha Joshi <69041055+NishaJoshi666@users.noreply.github.com>
2021-07-20 13:49:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,32 @@
-import cv2
+import os
 import dropbox
-import random
-import time
-startTime = time.time()
+from dropbox.files import WriteMode
 
-def take_picture():
-    number = random.randint(0,100)
-    videoPicture = cv2.VideoCapture(0)
-    result = True
-    while(result):
-        ret,frame = videoPicture.read()
-        imageName = 'picture'+ str(number) + '.png'
-        cv2.imwrite(imageName,frame)
-        result = False
-    return imageName
-    videoPicture.release()
-    cv2.destroyAllWindows()
-    print('picture has been captured')
+class TransferData:
+    def __init__(self,access_token):
+        self.access_token =  access_token
 
-def uploadPics(imageName):
-    access_token = '_kdXQaWORScAAAAAAAAAAev3fi46cAMrii2jyYnETXDzuErkYiox6TyM0gDwssMe'
-    filefrom = imageName
-    fileto = '/Project102/'+imageName
-    dbx = dropbox.Dropbox(access_token)
-    with open(filefrom,'rb') as f:
-        dbx.files_upload(f.read(),fileto,mode = dropbox.files.WriteMode.overwrite)
-        print('file has been uploaded')
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        for root, dirs, files in os.walk(file_from):
+
+            for filename in files:
+                local_path = os.path.join(root, filename)
+
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
 
 def main():
-    while(True):
-        if((time.time()-startTime)>=300and(time.time()-startTime)%300==0):
-            name = take_snapshot()
-            uploadPics(name)
+    access_token = 'riFu6Ybhc9AAAAAAAAAAHWkfE9AiGyD6n4254tOxesw7ShRjGjFhrjhRVa3NX3mx'
+    transferData = TransferData(access_token)
+
+    file_from = str(input("Enter the folder path to transfer : -"))
+    file_to = input("enter the full path to upload to dropbox:- ")
+
+    transferData.upload_file(file_from,file_to)
+    print("file has been moved !!!")
 
 main()
